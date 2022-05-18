e7d90b8073357a3e173a4e0777b7ea9d376a4b23
Cronopio2020 <alejandrojuarezprogrammer@live.com>
2022-05-18 11:19:07 -0500

    Comentarios

---
+++ Servicio.java
@@ -0,0 +1,34 @@
+
+//Comentarios para Git
+
+public class Servicio {
+	List<Persona> lista;
+	
+	public Servicio() {
+		lista = new ArrayList<>();
+	}
+	
+	public void addPersona(Persona p) {
+		lista.add(p);
+	}
+	
+	public void editarPersona(Persona p) {
+		for(int i=0; i < lista.size(); i++) {
+			if (lista.get(i).getDni().equalsIgnoreCase(p.getDni())) {
+				lista.set(i, p);
+			}
+		}		
+	}
+
+	public void eliminarPersona(Persona p) {
+		for(int i=0; i < lista.size(); i++) {
+			if (lista.get(i).getDni().equalsIgnoreCase(p.getDni())) {
+				lista.remove(i);
+			}
+		}		
+	}
+	
+	public List<Persona> listarTodasPersonas() {
+		return lista;
+	}	
+}
